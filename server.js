8c3afc6920135fae6f65059dbec1ac5b29f8e80d
Mont0360 <mont0360@umn.edu>
2020-06-16 21:36:29 -0500

    server.js update files fixed

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
+let express = require("express");
+let path = require("path");
+let fs = require("fs");
 
-const express = require("express");
-const path = require("path");
-const fs = require("fs");
+let app = express();
 
-// set up Express
-
-const app = express();
 const PORT = process.env.PORT || 3000;
-let data = JSON.parse(fs.readFileSync("/db/db.json", "utf8"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.get("/notes", function(req, res){
+
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+app.get("/api/notes", function(req, res){
+
+    res.sendFile(path.join(__dirname, "/db/db.json"));
+});
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+  });
